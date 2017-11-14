61dec43cf2f630bdaf2ece7b95e365946c3f17ff
julien-lav <julien.laville@hotmail.fr>
2017-11-14 13:29:51 +0100

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20171114101656) do
 
   create_table "users", force: :cascade do |t|
     t.string "phone"
+    t.string "string"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
